53f088fa32d3f0479d266f44d1632781569c70ab
leegieyoung <44660253+leegieyoung@users.noreply.github.com>
2021-02-08 16:02:32 +0900

    Update PBS_standard_mutect2-mutect2-TM-haplotypecaller_java64g-threads64_metastasis.sh
    Normal, tumor 둘 다 진행해주어야 함 (위 코드는 tumor 기준으로 드림)
    01. FASTQC
    02-1. Trimmomatic
    02-2. Check FASTQC
    03. BWA
    04. MarkDuplicate
    05. BQSR
    09. GetPileupSummaries
    이후 
    force_calling_mode
    mutect2_matched_normal
    mutect2_tumor_only_mode 
    
    진행

--- PBS_standard_mutect2-mutect2-TM-haplotypecaller_java64g-threads64_metastasis.sh
+++ PBS_standard_mutect2-mutect2-TM-haplotypecaller_java64g-threads64_metastasis.sh
@@ -7,20 +7,16 @@ fi
 
 sample=$1
 tumor=$2
-path="/scratch/x1997a11/WES/CTC"
-temp="/home01/x1997a11/temp"
-REFERENCE="/scratch/x1997a11/WES/REFERENCE/human_g1k_v37.fasta/human_g1k_v37/human_g1k_v37.fasta"
-SNP="/scratch/x1997a11/WES/REFERENCE/human_g1k_v37.fasta/SNP/Homo_sapiens_assembly19.dbsnp138.vcf"
-INDEL="/scratch/x1997a11/WES/REFERENCE/human_g1k_v37.fasta/INDEL/Homo_sapiens_assembly19.known_indels_20120518.vcf"
-INTERVAL="/scratch/x1997a11/WES/CTC/S04380110_Covered.bed"
-custom_PON="/scratch/x1997a11/WES/REFERENCE/human_g1k_v37.fasta/mutect2_pon/somatic-b37_Mutect2-exome-panel.vcf"
-germline_resource="/scratch/x1997a11/WES/REFERENCE/human_g1k_v37.fasta/mutect2_germline-resource/af-only-gnomad.raw.sites.b37.vcf.gz"
-#gnomAD="/scratch/x1997a11/WES/REFERENCE/human_g1k_v37.fasta/mutect2_germline-resource/af-only-gnomad.raw.sites.b37_biallelic.vcf"
-#gnomAD="/scratch/x1997a11/WES/REFERENCE/human_g1k_v37.fasta/mutect2_germline-resource/somatic-b37_small_exac_common_3.vcf"
-gnomAD="/scratch/x1997a11/WES/REFERENCE/human_g1k_v37.fasta/mutect2_gnomAD_chr_sites/gnomad.exomes.r2.1.1.sites_biallelic.vcf.gz"
-gnomAD_interval_list="/scratch/x1997a11/WES/REFERENCE/human_g1k_v37.fasta/mutect2_gnomAD_chr_sites/exome_calling_regions.v1.interval_list"
-
-echo -e "\n\n start sh : $0 \n Blood : $1 \n\n Kid : $2 \n\n"
+path="/WES/CTC"
+temp="/temp"
+REFERENCE="human_g1k_v37.fasta"
+SNP="Homo_sapiens_assembly19.dbsnp138.vcf"
+INDEL="Homo_sapiens_assembly19.known_indels_20120518.vcf"
+INTERVAL=S04380110_Covered.bed"
+custom_PON="somatic-b37_Mutect2-exome-panel.vcf"
+germline_resource="af-only-gnomad.raw.sites.b37.vcf.gz"
+gnomAD="gnomad.exomes.r2.1.1.sites_biallelic.vcf.gz"
+gnomAD_interval_list="exome_calling_regions.v1.interval_list"
 
 
 #01.FASTQC
@@ -62,23 +58,67 @@ gatk --java-options "-Xmx64G" ApplyBQSR -R ${REFERENCE} -I ${path}/${tumor}/04_M
 
 
 #--------------------------------------mutect2 common tool--------------------------------
-echo -e "\n \n gatk GetPileupSummaries Kid start \n \n"
+echo -e "\n \n gatk GetPileupSummaries tumor start \n \n"
 
 #09-GetPileupSummaries 
-#-V에 긴서열을 넣었음.
 mkdir ${path}/${sample}/09_${tumor}-pileups 
 gatk  --java-options "-Xmx64G" GetPileupSummaries -I ${path}/${tumor}/05_BQSR/${tumor}.sorted.markedup.recal.bam -V ${gnomAD} -L ${gnomAD_interval_list} -O ${path}/${sample}/09_${tumor}-pileups/${tumor}-pileups.table --tmp-dir ${temp}
 
+echo -e "\n \n gatk GetPileupSummaries B\n \n"
+mkdir ${path}/${sample}/temp-09_${sample}-pileups
+gatk GetPileupSummaries -I ${path}/${sample}/05_BQSR/${sample}.sorted.markedup.recal.bam -V ${gnomAD} -L ${gnomAD} -O ${path}/${sample}/temp-09_${sample}-pileups/${sample}-pileups.table --tmp-dir ${temp}
 
 #----------------------------------------force_calling_mode-----------------------------------
-#PBS
-qsub /scratch/x1997a11/PBS_Query/force_calling_mode/${tumor}-mutect2-fc.sh
+#08-Mutect2-force_calling_mode-mode
+echo -e "\n\n CTC-B mutect2-force-calling-mode start\n\n"
+
+mkdir ${path}/${sample}/08_${tumor}-Mutect2-PON-force_calling_mode
+gatk --java-options "-Xmx64G" Mutect2 -R ${REFERENCE} -I ${path}/${tumor}/05_BQSR/${tumor}.sorted.markedup.recal.bam --germline-resource ${germline_resource} --af-of-alleles-not-in-resource 0.00000005 --panel-of-normals ${custom_PON} -L ${INTERVAL} --native-pair-hmm-threads 64 -O ${path}/${sample}/08_${tumor}-Mutect2-PON-force_calling_mode/${tumor}-customPON-somatic-force_calling_mode.vcf.gz --dont-use-soft-clipped-bases true --force-active true --active-probability-threshold 0.001 --max-reads-per-alignment-start 0 --min-base-quality-score 10  --pair-hmm-implementation FASTEST_AVAILABLE -bamout ${path}/${sample}/08_${tumor}-Mutect2-PON-force_calling_mode/${tumor}-customPON-somatic-force_calling_mode.bam
+
+echo -e "\n\n CTC-B mutect2-force-calling-mode end\n\n"
+
+#10-CalculateContamination
+mkdir ${path}/${sample}/10_${tumor}-calculatecontamination-force_calling_mode
+gatk --java-options "-Xmx64G" CalculateContamination -I ${path}/${sample}/09_${tumor}-pileups/${tumor}-pileups.table --tumor-segmentation ${path}/${sample}/10_${tumor}-calculatecontamination-force_calling_mode/${tumor}-segment.tsv -O ${path}/${sample}/10_${tumor}-calculatecontamination-force_calling_mode/${tumor}-contamination.table
+echo -e "\n\nCalaulateContamination complete\n\n "
+
+#11-FilterMutectCalls
+mkdir ${path}/${sample}/11_${tumor}-FilterMutectCalls-force_calling_mode
+gatk --java-options "-Xmx64G" FilterMutectCalls -R ${REFERENCE} -V ${path}/${sample}/08_${tumor}-Mutect2-PON-force_calling_mode/${tumor}-customPON-somatic-force_calling_mode.vcf.gz --contamination-table ${path}/${sample}/10_${tumor}-calculatecontamination-force_calling_mode/${tumor}-contamination.table --tumor-segmentation ${path}/${sample}/10_${tumor}-calculatecontamination-force_calling_mode/${tumor}-segment.tsv -O ${path}/${sample}/11_${tumor}-FilterMutectCalls-force_calling_mode/${tumor}-filtermutectcalls-force_calling_mode.vcf.gz
+echo -e "\n\n Filtermutectcalls complete \n\n"
+
 #--------------------------------------mutect2 matched normal--------------------------------
-#PBS
 echo -e "\n \n gatk Mutect2 NM start \n \n"
-qsub /scratch/x1997a11/PBS_Query/Matched_normal/${sample}_${tumor}-mutect2-MN.sh
-#--------------------------------------mutect2 tumor only mode Kid--------------------------------
-#PBS
-echo -e "\n \n gatk Mutect2 tm Kid start \n \n"
-qsub /scratch/x1997a11/PBS_Query/tumor_only_mode/${tumor}-mutect2-tm.sh
+#08-Mutect2
+mkdir ${path}/${sample}/08_${tumor}Mutect2-force-calling-mode-MN
+gatk --java-options "-Xmx64G" Mutect2 -R ${REFERENCE} -I ${path}/${tumor}/05_BQSR/${tumor}.sorted.markedup.recal.bam -tumor tumor  -I ${path}/${sample}/05_BQSR/${sample}.sorted.markedup.recal.bam -normal blood  --germline-resource ${germline_resource} --af-of-alleles-not-in-resource 0.000001 --panel-of-normals ${custom_PON} -L ${INTERVAL} -dont-use-soft-clipped-bases true --force-active true --active-probability-threshold 0.001 --max-reads-per-alignment-start 0 --min-base-quality-score 10 --pair-hmm-implementation FASTEST_AVAILABLE --bam-writer-type ALL_POSSIBLE_HAPLOTYPES -bamout ${path}/${sample}/08_${tumor}Mutect2-force-calling-mode-MN/${tumor}-customPON-somatic-Mutect2-force-calling-mode-MN.bam --initial-tumor-lod 1.0 --native-pair-hmm-threads 64 -O ${path}/${sample}/08_${tumor}Mutect2-force-calling-mode-MN/${tumor}-customPON-somatic-Mutect2-force-calling-mode-MN.vcf.gz
+
+#10-CalculateContamination
+echo -e "\n \n calculationContamination start \n \n"
+mkdir ${path}/${sample}/10_${tumor}-calculatecontamination-force-calling-mode-MN
+gatk --java-options "-Xmx64G" CalculateContamination -I ${path}/${sample}/09_${tumor}-pileups/${tumor}-pileups.table -matched ${path}/${sample}/09_${sample}-pileups/${sample}-pileups.table --tumor-segmentation ${path}/${sample}/10_${tumor}-calculatecontamination-force-calling-mode-MN/${tumor}-segment.tsv -O ${path}/${sample}/10_${tumor}-calculatecontamination-force-calling-mode-MN/${tumor}-contamination.table
+
+echo -e "\n \n filtermutectcalls start \n \n"
+#11-FilterMutectCalls-force-calling-mode-MN
+mkdir ${path}/${sample}/11_${tumor}-FilterMutectCalls-force-calling-mode-MN
+gatk --java-options "-Xmx64G" FilterMutectCalls -R ${REFERENCE} -V ${path}/${sample}/08_${tumor}Mutect2-force-calling-mode-MN/${tumor}-customPON-somatic-Mutect2-force-calling-mode-MN.vcf.gz --contamination-table ${path}/${sample}/10_${tumor}-calculatecontamination-force-calling-mode-MN/${tumor}-contamination.table --tumor-segmentation ${path}/${sample}/10_${tumor}-calculatecontamination-force-calling-mode-MN/${tumor}-segment.tsv -O ${path}/${sample}/11_${tumor}-FilterMutectCalls-force-calling-mode-MN/${tumor}-filtermutectcalls.vcf.gz
+
+#--------------------------------------mutect2 tumor only mode --------------------------------
+#08-Mutect2-tumor-only-mode
+mkdir ${sample}/08_${tumor}-Mutect2-PON-tumor-only
+gatk Mutect2 -R ${REFERENCE} -I ${tumor}/05_BQSR/${tumor}.sorted.markedup.recal.bam --germline-resource ${germline_resource} --af-of-alleles-not-in-resource 0.0000000008588 --panel-of-normals ${custom_PON} -L ${INTERVAL} --native-pair-hmm-threads 4 -O ${sample}/08_${tumor}-Mutect2-PON-tumor-only/${tumor}-customPON-somatic-tumor-only.vcf.gz
+
+echo " mutect2-tumor-only-mode complete "
+
+#09-GetPileupSummaries 
+
+#10-CalculateContamination
+mkdir ${sample}/10_${tumor}-calculatecontamination-tumor-only
+gatk CalculateContamination -I ${sample}/09_${tumor}-pileups/${tumor}-pileups.table --tumor-segmentation ${sample}/10_${tumor}-calculatecontamination-tumor-only/${tumor}-segment.tsv -O ${sample}/10_${tumor}-calculatecontamination-tumor-only/${tumor}-contamination.table
+echo "CalaulateContamination complete "
+
+#11-FilterMutectCalls
+mkdir ${sample}/11_${tumor}-FilterMutectCalls-tumor-only
+gatk FilterMutectCalls -R ${REFERENCE} -V ${sample}/08_${tumor}-Mutect2-PON-tumor-only/${tumor}-customPON-somatic-tumor-only.vcf.gz --contamination-table ${sample}/10_${tumor}-calculatecontamination-tumor-only/${tumor}-contamination.table --tumor-segmentation ${sample}/10_${tumor}-calculatecontamination-tumor-only/${tumor}-segment.tsv -O ${sample}/11_${tumor}-FilterMutectCalls-tumor-only/${tumor}-filtermutectcalls-tumor-only.vcf.gz
+echo " Filtermutectcalls complete "
 
